9d9895371fba51fbc1f834cf35935fc4e3e78018
Quentin Vanhauteghem <quentvht@hotmail.com>
2026-02-21 18:01:03 +0100

    improved gate script

--- gate/src/main.cpp
+++ gate/src/main.cpp
@@ -8,48 +8,38 @@
 #include <Preferences.h>
 #include <ArduinoJson.h>
 #include <Wire.h> 
-#include <Adafruit_AHTX0.h> 
-#include <Adafruit_BMP280.h>
+#include <Adafruit_NeoPixel.h>
 
-#define PIN_RELAY_TOGGLE     2
+#define PIN_RELAY_TOGGLE     0
+
+#define MQTT_CLIENT_ID "gate2-toggle-1"
 
 const unsigned long RELAY_PULSE_MS = 500; //ms
 
 WiFiClient espClient;
-PubSubClient mqttClient(espClient);
 Preferences prefs;
-
-String mqttServer = "";
-uint16_t mqttPort = 1883;
-String mqttTopic = "gate/state";
-uint16_t mqttDelay = 30; // milli
-
-unsigned long lastMqttPublish = 0;
 
 WiFiManager wm;
 AsyncWebServer server(80);
 
-Adafruit_AHTX0 aht; 
-Adafruit_BMP280 bmp;
+
+Adafruit_NeoPixel pixels(1, GPIO_NUM_10, NEO_GRB + NEO_KHZ800);
 
 void setupWiFi() {
   WiFi.mode(WIFI_AP_STA);
+  esp_wifi_set_max_tx_power(30);
 
   wm.setConfigPortalBlocking(true);
   wm.setDebugOutput(true);
 
-  bool res = wm.autoConnect("GateConfig", "GateConfig123");
+  bool res = wm.autoConnect("GarageDoorConfig", "GarageDoorConfig123");
 
   if (!res) {
+    pixels.setPixelColor(0, pixels.Color(255, 0, 0));
+    pixels.show();
     delay(3000);
     ESP.restart();
   }
-}
-
-void loadMqttConfig() {
-  mqttServer = prefs.getString("server", "");
-  mqttPort   = prefs.getUShort("port", 1883);
-  mqttTopic  = prefs.getString("topic", "gate/state");
 }
 
 void setupWebServer() {
@@ -64,66 +54,6 @@ void setupWebServer() {
       request->send(200, "application/text", "");
     }
   );
-
-  server.on("/state", HTTP_GET, [](AsyncWebServerRequest *request) {
-      sensors_event_t humidity, temp;
-      aht.getEvent(&humidity, &temp);
-      
-      JsonDocument resp;
-      resp["hum"] = String(humidity.relative_humidity);
-      resp["aht_temp"] = String(temp.temperature);
-      resp["bmp_temp"] = String(bmp.readTemperature());
-      resp["pressure"] = String(bmp.readPressure() / 100.0F);
-
-      String out; 
-      serializeJson(resp, out);
-      request->send(200, "application/json", out);
-  });
-
-  server.on("/config/mqtt", HTTP_GET, [](AsyncWebServerRequest *request) {
-      JsonDocument resp; 
-      resp["server"] = mqttServer; 
-      resp["port"] = mqttPort; 
-      resp["topic"] = mqttTopic; 
-      resp["delay"] = mqttDelay;
-      String out; 
-      serializeJson(resp, out);
-      request->send(200, "application/json", out);
-    }
-  );
-
-  // {"server":"192.168.1.10","port":1883,"topic":"gate/state"}
-  server.on("/config/mqtt", HTTP_POST, [](AsyncWebServerRequest *request){}, NULL, [](AsyncWebServerRequest *request, uint8_t *data, size_t len, size_t index, size_t total) {
-      JsonDocument doc;
-      DeserializationError err = deserializeJson(doc, data, len);
-
-      if (err) {
-        request->send(400, "application/json", "{\"error\":\"invalid JSON\"}");
-        return;
-      }
-
-      if (!doc.containsKey("server") ||
-          !doc.containsKey("port") ||
-          !doc.containsKey("topic")) {
-        request->send(400, "application/json", "{\"error\":\"missing fields\"}");
-        return;
-      }
-
-      mqttServer = doc["server"].as<String>();
-      mqttPort   = doc["port"].as<uint16_t>();
-      mqttTopic  = doc["topic"].as<String>();
-      mqttDelay  = doc["delay"].as<uint16_t>();
-
-      prefs.putString("server", mqttServer);
-      prefs.putUShort("port", mqttPort);
-      prefs.putString("topic", mqttTopic);
-      prefs.putUShort("delay", mqttDelay);
-
-      mqttClient.setServer(mqttServer.c_str(), mqttPort);
-
-      request->send(200, "application/text", "");
-    }
-  );
   
   server.on("/reset/wifi", HTTP_GET, [](AsyncWebServerRequest *req){
       req->send(200, "text/plain", "Ok");
@@ -133,79 +63,33 @@ void setupWebServer() {
   );
 
   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send(200, "text/plain", "Available api: GET /reset/wifi ; POST /state/toggle; GET /state ; POST /config/mqtt {'server':'192.168.1.10','port':1883,'topic':'gate/state'}");
+    request->send(200, "text/plain", "Available api: GET /reset/wifi ; POST /state/toggle; POST /config/mqtt {'server':'192.168.1.10','port':1883,'topic':'garagedoor/state'}");
   });
 
   server.begin();
+
+  pixels.setPixelColor(0, pixels.Color(0, 0, 255));
+  pixels.show();
 }
 
-void ensureMqttConnected() {
-  if (mqttServer.length() == 0) return;
-
-  if (mqttClient.connected()) return;
-
-  String clientId = "gate-toggle-1";
-  if (!mqttClient.connect(clientId.c_str())) {
-    delay(60000);
-  }
-}
-
-void publishMqttState() {
-  if (mqttServer.length() == 0) return;
-  if (!mqttClient.connected()) {
-    ensureMqttConnected();
-  }
-
-  sensors_event_t humidity, temp;
-  aht.getEvent(&humidity, &temp);
-
-  float pressure = bmp.readPressure() / 100.0F; // hPa
-
-  String payload = "{";
-  payload +=  "\"temp\":\"" + String(temp.temperature) + "\",";
-  payload +=  "\"pressure\":\"" + String(pressure) + "\",";
-  payload +=  "\"humidity\":\"" + String(humidity.relative_humidity) + "\"";
-  payload += "}";
-
-  mqttClient.publish(mqttTopic.c_str(), payload.c_str());
-}
 
 void setup() {
-  delay(2000);
+  Serial.begin(115200);
+  delay(5000);
   
   pinMode(PIN_RELAY_TOGGLE, OUTPUT);
   digitalWrite(PIN_RELAY_TOGGLE, LOW);
 
-  Wire.begin(9, 8);
 
-  aht.begin();
-  bmp.begin(0x77);
-
-  prefs.begin("mqtt", false);
-  loadMqttConfig();
-
+  pixels.begin();
+  pixels.setBrightness(1);
+  pixels.setPixelColor(0, pixels.Color(0, 255, 0));
+  pixels.show();
+  
   setupWiFi();
-
-  if (mqttServer.length() > 0) {
-    mqttClient.setServer(mqttServer.c_str(), mqttPort);
-  }
-
-  setupWebServer();
+  
+  setupWebServer();  
 }
 
 void loop() {
-  if (mqttServer.length() == 0) {
-    return;
-  }
-
-  if (!mqttClient.connected()) {
-    ensureMqttConnected();
-  }
-  mqttClient.loop();
-
-  unsigned long now = millis();
-  if (now - lastMqttPublish >= mqttDelay) {
-    publishMqttState();
-    lastMqttPublish = now;
-  } 
 }
